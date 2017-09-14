b79d33fefd5fe7ff704ca69a1755e444a92ed5c6
Marcela Crosariol <marcela@crosariol.com.br>
2017-09-14 13:39:10 -0300

    Adicionadas as funções: getUserProfile, changeNotifications e appStatistics

--- Django Proj/webapp/experiment/views.py
+++ Django Proj/webapp/experiment/views.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.core.mail import send_mail
 from django.contrib.auth.models import User
 from experiment.forms import ExecutionForm, ContactForm
-from experiment.models import Execution, Algorithms, UsuarioFriends
+from experiment.models import Execution, Algorithm, AppUser
 from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from django.core.urlresolvers import reverse
@@ -15,15 +15,14 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from jsonview.decorators import json_view
 from crispy_forms.utils import render_crispy_form
 from crispy_forms.helper import FormHelper
-
+from django.contrib import messages
 # paginator
-from paginator import paginate
+from experiment.paginator import paginate
 
 from experiment.tasks import RunExperiment
 
 def about(request):
     return HttpResponse(1)
-
 
 def home(request):
     if not request.user.is_authenticated():
@@ -40,13 +39,13 @@ def home(request):
         executionList = Execution.objects.filter(
             request_by__usuario__id=request.user.id).order_by('-id')
         try:
-            UserProf = UsuarioFriends.objects.get(usuario__id=request.user.id)
+            UserProf = AppUser.objects.get(usuario__id=request.user.id)
         except:
-            print "Erro. Criando novo userProf"
+            print ("Erro. Criando novo userProf")
             user = User.objects.get(id=request.user.id)
-            UserProf = UsuarioFriends(usuario=user)
+            UserProf = AppUser(usuario=user)
             UserProf.save()
-            print "Criado novo UserProf"
+            print ("Criado novo UserProf")
         paginator = Paginator(executionList, UserProf.resultsPerPage)
         page = request.GET.get('page')
         if page is None:
@@ -73,6 +72,21 @@ def home(request):
 def about(request):
     return render(request, "about.html", {})
 
+def getUserProfile(request, username):
+    user = User.objects.get(username=username)
+    userFriend = AppUser.objects.get(usuario=user.id)
+    return render(request, 'user_profile.html', {"user":user, "userFriend":userFriend})    
+
+def changeNotifications(request, username):
+    choice = request.POST.get("choice")
+
+    user = User.objects.get(username=username)
+    userFriend = AppUser.objects.get(usuario=user.id)
+
+    userFriend.notification = choice
+    userFriend.save()
+
+    return HttpResponseRedirect(reverse('userProfile', kwargs={'username':username}))
 
 def downloadInputFile(request):
     expId = request.GET.get('id')
@@ -91,19 +105,18 @@ def downloadInputFile(request):
     return response
     # return HttpResponseRedirect(reverse('home'))
 
-
 def downloadOutputFile(request):
     expId = request.GET.get('id')
     execution = Execution.objects.get(pk=expId)
     if (execution.request_by.usuario.id == request.user.id):
-        print execution.outputFile.url
-        print "Autorizado"
+        print (execution.outputFile.url)
+        print ("Autorizado")
         response = HttpResponse(
             execution.outputFile, content_type='application/force-download')
         response[
             'Content-Disposition'] = 'attachment; filename="Resultado-Experimento-' + str(expId) + '"'
         return response
-    print "Nao autorizado"
+    print ("Nao autorizado")
     # criar alerta
     return HttpResponseRedirect(reverse('home'))
 
@@ -113,9 +126,10 @@ def contact(request):
     if form.is_valid():
         subject = 'Portal Friends - Mensagem de %s ' % (
             form.cleaned_data.get("nome"))
-        from_email = settings.EMAIL_HOST_USER  # da pra usar outro?
+        from_email = settings.EMAIL_HOST_USER  
         to_email = from_email
-        message = form.cleaned_data.get("mensagem")
+        sender = form.cleaned_data.get("email")
+        message = "Contact: " + sender + "\n" + form.cleaned_data.get("mensagem")
         send_mail(subject,
                   message,
                   from_email,
@@ -126,16 +140,14 @@ def contact(request):
     }
     return render(request, "contact.html", context)
 
-
 # ajax
-
 
 @json_view
 @csrf_protect
 def checkForm(request):
     form = ExecutionForm(request.POST or None)  # request POST?
     print(request.POST)
-    print "\n\n"
+    print ("\n\n")
     if form.is_valid():  # processa
         experiments(request)
         helper = FormHelper()
@@ -160,20 +172,20 @@ def experiments(request):
             # form_html = render_crispy_form(form)
             context = {
                 "form": form,
-                'title': title
+                'title': title,
             }
             return render(request, "experiments.html", context)
-        algorithm = request.POST.get('Algorithm')
+        algorithm = request.POST.get('Algoritmo')
         d_User = User.objects.get(username=request.user)
-        alg = Algorithms.objects.get(nameAlg=algorithm)
+        alg = Algorithm.objects.get(nameAlg=algorithm)
         execution = Execution(
-            request_by=d_User.usuariofriends,
-            algorithm=alg,
+            request_by=d_User.appuser,
+            algorithm=alg
         )
         execution.save()
         if (request.FILES):
             # print request.FILES
-            fileIn = request.FILES["Input"]
+            fileIn = request.FILES["Entrada"]
             execution.inputFile = fileIn
             execution.save()
             queryInputFile = (
@@ -187,7 +199,7 @@ def experiments(request):
             # print query
         else:
             query = execution.algorithm.command
-        outputFilePath = './users/user_' + \
+            outputFilePath = './users/user_' + \
             str(execution.request_by.usuario.id) + \
             '/' + str(execution.id) + '/output'
         # print(outputFilePath)
@@ -209,7 +221,7 @@ def experiments(request):
     title = "Experiments %s" % (request.user)
     context = {
         "title": title,
-        "form": form
+        "form": form,
     }
     return render(request, "experiments.html", context)
 
@@ -218,25 +230,42 @@ def experimentsRemove(request):
         data = request.POST.get('data')
         if data:
             ids = data.split(",")
-            print ids
+            print (ids)
             Execution.objects.filter(id__in=ids).delete()
         # objects = Model.objects.filter(id__in=object_ids)
     return HttpResponseRedirect(reverse('home'))
 
+def appStatistics(request):
+    context = {}
+    return render(request, "statistics.html", context)
+
 @csrf_exempt
 def result(request):
-    print "testePRINT"
     if request.method == 'POST':
-        print "POST"
-        if (request.FILES):
-	    idExec = request.POST.get("id")
-	    tempo  = request.POST.get("time")
-	    print idExec
-	    print tempo
-	    execution = Execution.objects.get(id=idExec)
-	    fileIn = request.FILES["file"]
-	    execution.outputFile=fileIn
-	    execution.status=3
-	    execution.time = tempo
+        print ("POST")
+    if (request.FILES):
+            idExec = request.POST.get("id")
+            tempo  = request.POST.get("time")
+            print("id: %s time: %s" %(idExec,tempo))
+
+            execution = Execution.objects.get(id=idExec)
+            fileIn = request.FILES["file"]
+            execution.outputFile=fileIn
+            execution.status=3
+            execution.time = tempo
             execution.save()
+
+            userFriend = execution.request_by
+            userEmail = userFriend.usuario.email
+
+            if (userFriend.notification == 'yes'): 
+                subject = 'Portal Friends - Experimento concluido com sucesso' 
+                from_email = settings.EMAIL_HOST_USER  
+                to_email = userEmail
+                message = "Olá " + userFriend.nickname + " experiencia " + idExec + " foi concluida com sucesso"
+                send_mail(subject, message,from_email,[to_email], fail_silently=False)
+
     return HttpResponse(1)
+
+def register_sucess(request):
+    return render(request, "registration/registration_complete.html", {})
